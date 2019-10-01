caab65e5bee796807195772bef4dca3e41457074
Jair Rolón <35773865+Jair24@users.noreply.github.com>
2019-10-01 17:56:49 -0500

    Update coladera.cpp

--- coladera.cpp
+++ coladera.cpp
@@ -1,1 +1,41 @@
-#include <stdio.h>
+#include<iostream>
+
+using namespace std;
+
+void tabladeprimos(int primo[],int n);
+void Multiplos(int primo[],int n);
+void show(int primo[],int n);
+
+int main()
+{
+	int primo[1000];
+	
+	tabladeprimos(primo,1000);
+	
+	Multiplos(primo,1000);
+	
+	show(primo,1000);
+}
+void tabladeprimos(int primo[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+  	primo[i]=true;
+	}
+}
+void Multiplos(int primo[],int n)
+{
+	for(int i=2;i<n;i++){
+	for(int j=2;i*j<n;j++){
+		primo[i*j]=false;
+		}
+	}
+}
+void show(int primo[],int n)
+{
+	for(int i=2;i<n;i++)
+	{
+	if(primo[i])
+	cout<<i<<" ";
+	}
+}  
